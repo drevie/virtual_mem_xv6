cb3d074f3175ed3d8070cce7f4f8adb8a8cb4391
Daniel Revie <daniel.revie1@gmail.com>
2016-12-08 14:37:25 -0500

    hey

--- vm.c
+++ vm.c
@@ -421,7 +421,7 @@ copyout(pde_t *pgdir, uint va, void *p, uint len)
   // structure to hold sharing information
   struct entry
   {
-    /
+    
     int count;
   } share_tbl[60 * 1024]; // table for all pages, upto 240MB(PHYSTOP)
 
